e55028f0c7332c9d810c5388fa35447225d45d13
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-03-17 23:46:01 +0900

    [Add]: 동영상 추가 및 notion 이미지 대체

--- public/Portfolio.js
+++ public/Portfolio.js
@@ -6,8 +6,15 @@ function Portfolio() {
   this.render = () => {
     $portfolio.innerHTML = `
     <div class="modal-contents">
-      <li class="modal-closeBtn">닫기</li>
-      <h2>KKABAL - IT 부트캠프 정보 공유 웹 커뮤니티</h2>
+      <li class="modal-closeBtn">페이지 닫기</li>
+      <div class="modal-title">
+        <h2>KKABAL - IT 부트캠프 정보 공유 웹 커뮤니티</h2>
+        <a href="https://github.com/kimbigmin/KKABLA" target="_blank">GitHub주소 : https://github.com/kimbigmin/KKABLA</a>
+      </div>
+      <video width="80%" autoplay controls muted>
+        <source src="./video/kkabla.mp4" type="video/mp4">
+      </video>
+      <img src="./image/kkabla_notion.png"/>
     </div>
   `;
   };
