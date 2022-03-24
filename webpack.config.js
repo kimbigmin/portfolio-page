f1008123236cb9cfb038328924695b98fb438f5c
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-03-25 03:42:35 +0900

    [Fix]: 빌드 에러 수정2

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-  },
   module: {
     rules: [
       {
